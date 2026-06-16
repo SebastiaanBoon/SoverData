53f524b0ddbe3bf9d79187ac65f478c1a59b4541
Sebastiaan Boon | Kasparov <sebastiaanboon@kasparov-bi.nl>
2026-06-16 15:53:34 +0200

    fix: use correct server entry point with trigger loop

--- startup.sh
+++ startup.sh
@@ -2,4 +2,4 @@
 # Generic startup script for SoverData container
 export PYTHONPATH=/app:$PYTHONPATH
 cd /app
-exec python -m uvicorn server.api.main:app --host 0.0.0.0 --port 8000 --workers 1
+exec python -m uvicorn server.main:app --host 0.0.0.0 --port 8000 --workers 1
